210f67687e218946bfa29916d3074f2930b9818c
Mikael Chudinov <mikael@chudinov.net>
2020-03-02 15:37:09 +0100

    Read blobvs.

--- Blob2EventHub/Blob2EventHub.cs
+++ Blob2EventHub/Blob2EventHub.cs
@@ -1,17 +1,35 @@
 using System;
 using System.IO;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
+[assembly: FunctionsStartup(typeof(Blob2EventHub.Blob2EventHub))]
 namespace Blob2EventHub
 {
-    public static class Blob2EventHub
+    public class Blob2EventHub : FunctionsStartup
     {
-        [FunctionName("Blob2EventHub")]
-        public static void Run([BlobTrigger("samples-workitems/{name}", Connection = "xxx")]Stream myBlob, string name, ILogger log)
+        public override void Configure(IFunctionsHostBuilder builder)
         {
-            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+            var configRoot = new ConfigurationBuilder()
+                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+        }
+
+        [FunctionName("Blob2EventHub")]
+        public static void Run([BlobTrigger("import-app-insights/{name}", Connection = "StorageConnection")]Stream blob, string name, ILogger log, ExecutionContext context)
+        {
+            try
+            {
+                log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {blob.Length} Bytes");
+            }
+            catch (Exception ex)
+            {
+                log.LogWarning($"{ex.Message}");
+            }            
         }
     }
 }
